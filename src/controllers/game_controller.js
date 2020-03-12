a79986a2495dd483a343001fdf8230d23bce37f9
João Harbs <harbsprog@gmail.com>
2020-03-11 22:07:16 -0300

    Add dependencies like sequelize and path and create sequelize structure for use sqlite db

--- src/controllers/game_controller.js
+++ src/controllers/game_controller.js
@@ -1,4 +1,6 @@
 'use strict'
+import { Games } from '../models'
+
 class GameController {}
 
 export default new GameController()
---
+++ src/database/migrations/20200312004910-create-games.js
@@ -0,0 +1,46 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, DataTypes) => {
+    return queryInterface.createTable('Games', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      game: {
+        allowNull: false,
+        type: DataTypes.INTEGER
+      },
+      total_kills: {
+        allowNull: false,
+        type: DataTypes.INTEGER
+      },
+      players: {
+        allowNull: true,
+        type: DataTypes.JSON
+      },
+      kills: {
+        allowNull: true,
+        type: DataTypes.JSON
+      },
+      log: {
+        allowNull: true,
+        type: DataTypes.JSON
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    })
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('Games')
+  }
+}
